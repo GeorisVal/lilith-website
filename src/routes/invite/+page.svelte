6684607cb3b2ddbaffd370f0b16a32c995640e63
Marco <marcoabricot@gmail.com>
2023-06-22 13:09:00 +0200

    Fixed 500 error on /invite

--- src/routes/invite/+page.svelte
+++ src/routes/invite/+page.svelte
@@ -1,7 +1,7 @@
 <script>
   import { browser } from '$app/environment';
 
-  if (!browser) {
+  if (browser) {
     window.location.href =
       'https://canary.discord.com/api/oauth2/authorize?client_id=1119566542628192340&permissions=17876191071232&scope=applications.commands%20bot';
   }
